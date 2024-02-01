6554b32bc9412beddbfdca7b9c1354cf1300e1ca
sameer-bagde <144990200+sameer-bagde@users.noreply.github.com>
2024-02-01 17:33:18 +0000

    node js day 1

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-async function readFileContent(filePath) {
-    try {
-        const content = await fs.readFile(filePath, 'utf-8');
-        
-        if (content.length === 0) {
-            console.log('File is empty.');
+function readFileContent(filePath) {
+    fs.readFile(filePath, 'utf-8', (error, content) => {
+        console.log(`\nTest Case: ${filePath}`);
+        if (error) {
+            console.error(`Error reading file: ${error.message}`);
         } else {
-            console.log('File Content:');
-            console.log(content);
+            if (content.length === 0) {
+                console.log("empty string)");
+            } else {
+                console.log('File Content:');
+                console.log(content);
+            }
         }
-    } catch (error) {
-        console.error(`Error reading file: ${error.code} - ${error.message}`);
-    }
+    });
 }
 
-(async () => {
-    console.log('Test Case 1: \n');
-    await readFileContent('test-files/file1.txt');
-    // Expected Output: Content of file1.txt
+// Test Case 1
+readFileContent('test-files/file1.txt');
 
-    console.log('\nTest Case 2: \n');
-    await readFileContent('test-files/empty-file.txt');
-    // Expected Output: (empty string)
+// Test Case 2
+readFileContent('test-files/empty-file.txt');
 
-    console.log('\nTest Case 3: \n');
-    await readFileContent('test-files/nonexistent-file.txt');
-    // Expected Output: Error reading file: ENOENT: no such file or directory...
-})();
+// Test Case 3
+readFileContent('test-files/nonexistent-file.txt');
