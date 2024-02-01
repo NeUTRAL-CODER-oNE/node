512b4784165ecfe6a3a590af6c5d67e042f03e64
sameer-bagde <144990200+sameer-bagde@users.noreply.github.com>
2024-02-01 17:14:27 +0000

    my code commit 2

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ async function readFileContent(filePath) {
 }
 
 (async () => {
-    console.log('Test Case 1:');
+    console.log('Test Case 1: \n');
     await readFileContent('test-files/file1.txt');
     // Expected Output: Content of file1.txt
 
-    console.log('\nTest Case 2:');
+    console.log('\nTest Case 2: \n');
     await readFileContent('test-files/empty-file.txt');
     // Expected Output: (empty string)
 
-    console.log('\nTest Case 3:');
+    console.log('\nTest Case 3: \n');
     await readFileContent('test-files/nonexistent-file.txt');
     // Expected Output: Error reading file: ENOENT: no such file or directory...
 })();
